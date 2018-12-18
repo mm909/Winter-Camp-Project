d7270eba997649c9973935e694bbef612e565d64
Mikian Musser <Bobar312@gmail.com>
2018-12-18 14:02:18 -0800

    Created base website

---
+++ typed/type.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'I',
+      'Love',
+      'Snow!'
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+  });
+  var type = new Typed('#cardTitle1', {
+    strings: [
+      'Test',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
